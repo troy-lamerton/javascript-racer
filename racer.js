791c05c865ece9db7da7894352a4cfc7a59708fb
troy-lamerton <troy.lamerton@gmail.com>
2016-04-14 19:20:14 +1200

    Add information to index.html

--- racer.js
+++ racer.js
@@ -4,12 +4,15 @@ var SPEED = 5;
 //players
 var player1 = document.getElementById('player1');
 var player2 = document.getElementById('player2');
-var players = [{style:player1.style, position:0}, {style: player2.style, position:0}];
+//ensure position = races.css .player{left: [% value]} 
+var players = [{style:player1.style, position:-9}, {style: player2.style, position:-9}];
+//var playersBackup = JSON.parse(JSON.stringify(players)); //clones players array
 
-//increases margin-left of the player element
+
+//move a player element to the right
 var movePlayer = function movePlayer (playerNum) {
   players[playerNum].position += SPEED;
-  players[playerNum].style["left"] = players[playerNum].position + "%";
+  players[playerNum].style["margin-left"] = players[playerNum].position + "%";
 };
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -17,11 +20,20 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function keyDownHandler (e) {
-  //P key
+  //R key is 82
+  if (e.keyCode == 82) {
+    restartGame();
+    return;
+  }
+  //Q key is 81, P key is 80
   if (e.keyCode == 81) {
     movePlayer(0);
   }
   else if (e.keyCode == 80) {
     movePlayer(1);
   }
+}
+
+function restartGame () {
+  location.reload();
 }
