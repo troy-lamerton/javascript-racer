7969e56d943d9e4b33d532f5c757df82d617feb5
troy-lamerton <troy.lamerton@gmail.com>
2016-04-14 18:13:50 +1200

    Add keydown handling for player movement

--- racer.js
+++ racer.js
@@ -1,5 +1,5 @@
 //constants
-var SPEED = 12;
+var SPEED = 5;
 
 //players
 var player1 = document.getElementById('player1');
@@ -7,7 +7,21 @@ var player2 = document.getElementById('player2');
 var players = [{style:player1.style, position:0}, {style: player2.style, position:0}];
 
 //increases margin-left of the player element
-var moves = function moves (playerNum) {
+var movePlayer = function movePlayer (playerNum) {
   players[playerNum].position += SPEED;
   players[playerNum].style["left"] = players[playerNum].position + "%";
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('keydown', keyDownHandler, false)
+})
+
+function keyDownHandler (e) {
+  //P key
+  if (e.keyCode == 81) {
+    movePlayer(0);
+  }
+  else if (e.keyCode == 80) {
+    movePlayer(1);
+  }
+}
